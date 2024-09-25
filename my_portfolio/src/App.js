05487f63e8dc550e33b8a4452ac3210e056496bd
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-09-25 06:06:41 +0300

    all sections

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
 import React from "react";
 import { Routes, Route, createRoutesFromElements, createBrowserRouter , RouterProvider, } from "react-router-dom";
+
+import  Home  from "./pages/Home";
+import  About  from "./pages/About";
+import  Projects  from "./pages/Projects";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<App />} />
+    </Route>
+  )
+)
 function App() {
   return (
     
