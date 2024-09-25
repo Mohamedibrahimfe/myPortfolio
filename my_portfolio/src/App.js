0eb4be0dc5074d28363713451f786ea4f4cd9985
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-09-25 06:04:22 +0300

    basic config

---
+++ my_portfolio/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route, createRoutesFromElements, createBrowserRouter , RouterProvider, } from "react-router-dom";
+function App() {
+  return (
+    
+  )
+}
+
+export default App;
