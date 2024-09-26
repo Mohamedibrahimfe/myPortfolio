00cfebe881dc0bc79ec29095b2fd51ddc594fe47
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-09-26 04:30:17 +0300

    sidebar and footer are finished

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 import {
@@ -11,17 +10,21 @@ import {
 import RootLayOut from "./Layout/RootLayOut";
 
 import Home from "./pages/Home";
+import Skills from "./pages/Skills";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import Links from "./pages/Links";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayOut />}>
       <Route index element={<Home />} />
+      <Route path="/skills" element={<Skills />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/links" element={<Links />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
