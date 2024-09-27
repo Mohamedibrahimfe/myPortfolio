350e8f553338e8dbbdc2b84e840412c57162e563
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-09-27 03:50:54 +0300

    about section

--- my_portfolio/src/pages/About.jsx
+++ my_portfolio/src/pages/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import aboutData from "../data/about.json";
+import "../style/About.css";
 
-const About = () => {
-  return <div>About</div>;
-};
+const About = () => (
+  <section className="about" id="about">
+    <h2>{aboutData.data.about.greeting}</h2>
+    <p>{aboutData.data.about.description}</p>
+
+    <br />
+    <p dangerouslySetInnerHTML={{ __html: aboutData.data.about.education }} />
+
+    <br />
+    <p
+      dangerouslySetInnerHTML={{
+        __html: aboutData.data.about.beliefAndGoal,
+      }}
+    />
+
+    <br />
+    <p
+      dangerouslySetInnerHTML={{
+        __html: aboutData.data.about.aboutAi,
+      }}
+    />
+  </section>
+);
 
 export default About;
