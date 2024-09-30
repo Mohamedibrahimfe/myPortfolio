a5f16d1921370c9d8b3a57edc3e1ed0777d45d24
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-10-01 00:44:54 +0300

    project section works

--- my_portfolio/src/pages/Projects.jsx
+++ my_portfolio/src/pages/Projects.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import "../style/Projects.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import data from "../data/about.json";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1 className="title">Projects</h1>
+      <Tabs>
+        <TabList>
+          <Tab>Pure Projects</Tab>
+          <Tab>React Projects</Tab>
+        </TabList>
+        <TabPanel>
+          {data.projects.map((project) => (
+            <div className="project">
+              <h3 className="project-title">{project.title}</h3>
+
+              <img src={project.image} />
+              <div className="project-technologies">
+                <p>{project.tech}</p>
+              </div>
+              <p>{project.description}</p>
+
+              <div className="links">
+                <a href={project.link} target="_blank" rel="noreferrer">
+                  GitHub
+                </a>
+                <a href={project.demo} target="_blank" rel="noreferrer">
+                  live demo
+                </a>
+              </div>
+            </div>
+          ))}
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Projects;
