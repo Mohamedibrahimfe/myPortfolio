30221ecf71e48f5d7c372d114eb4ee4a86ce38ee
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-11-01 15:17:45 +0200

    Update About.jsx

--- my_portfolio/src/pages/About.jsx
+++ my_portfolio/src/pages/About.jsx
@@ -5,56 +5,56 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import React, { useEffect } from "react";
 gsap.registerPlugin(ScrollTrigger);
 const About = () => {
-  // useEffect(() => {
-  //   gsap.fromTo(
-  //     "#about",
-  //     {
-  //       opacity: 0,
-  //       x: -100,
-  //     },
-  //     {
-  //       opacity: 1,
-  //       x: 0,
-  //       duration: 1,
-  //       scrollTrigger: {
-  //         trigger: "#about",
-  //         start: "top 80%",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
+  useEffect(() => {
+    gsap.fromTo(
+      "#about",
+      {
+        opacity: 0,
+        x: -100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: "#about",
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
 
-  //   gsap.fromTo(
-  //     ".right",
-  //     { opacity: 0, x: 100 },
-  //     {
-  //       opacity: 1,
-  //       x: 0,
-  //       duration: 1,
-  //       scrollTrigger: {
-  //         trigger: "#about",
-  //         start: "top 80%",
-  //         end: "top 80%",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
+    gsap.fromTo(
+      ".right",
+      { opacity: 0, x: 100 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: "#about",
+          start: "top 80%",
+          end: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
 
-  //   gsap.fromTo(
-  //     ".left",
-  //     { x: -100, opacity: 0 },
-  //     {
-  //       opacity: 1,
-  //       x: 0,
-  //       duration: 1,
-  //       scrollTrigger: {
-  //         trigger: "#about",
-  //         start: "top 80%",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
-  // })
+    gsap.fromTo(
+      ".left",
+      { x: -100, opacity: 0 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: "#about",
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+  });
   return (
     <section className="about" id="about">
       <h2 className="aboutTitle ">{aboutData.data.about.greeting}</h2>
