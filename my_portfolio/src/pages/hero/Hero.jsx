94d1608d21a4105cbcf54120d5d1aa947d6cc407
MOHAMED IBRAHIM <147580141+Mohamedibrahimfe@users.noreply.github.com>
2025-02-05 02:05:02 +0200

    cv downloadable

--- my_portfolio/src/pages/hero/Hero.jsx
+++ my_portfolio/src/pages/hero/Hero.jsx
@@ -32,6 +32,7 @@ const Home = (props) => {
     const localValue = localStorage.getItem("isChecked");
     if (localValue == null) return null;
   });
+
   const sections = [
     "#hero",
     "#about",
@@ -102,7 +103,7 @@ const Home = (props) => {
 
           <div className="cta-buttons">
             {" "}
-            <button className="btn" download={resume}>
+            <a className="btn exception" href={resume} download={resume}>
               <span className="transition"></span>
               <span className="gradient"></span>
               <svg
@@ -118,7 +119,7 @@ const Home = (props) => {
                 <path d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
               </svg>{" "}
               <span className="label ex">Download CV</span>
-            </button>
+            </a>
             <a className="btn exception" href="#contact">
               <span className="transition"></span>
               <span className="gradient"></span>{" "}
